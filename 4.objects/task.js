2cdefdc4a5cfc278fa90823f3a6e8945742a3ed1
Angry Baker <checheninlive@mail.ru>
2024-04-17 03:45:46 +0300

    hw 4 is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
+  if ("marks" in this === false) {
+    return;
+  } else {
+        this.marks.push(...marks);
+     }
   
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty("marks") === false || this.marks.length === 0 ) {
+        return 0;
+      } else {
+          return this.marks.reduce((acc, element) => acc + element) / this.marks.length;
+      } 
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
